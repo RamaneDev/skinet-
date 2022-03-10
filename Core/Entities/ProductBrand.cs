f00bd9b829f1bfe9ae256761cba69b34e0c871c9
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-03-11 00:26:46 +0100

    Extend Product Entity & Update database

---
+++ Core/Entities/ProductBrand.cs
@@ -0,0 +1,8 @@
+namespace Core.Entities
+{
+    public class ProductBrand : BaseEntities
+    {
+        public string Name { get; set; }
+
+    }
+}
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,6 +1,3 @@
-
-
-
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +8,10 @@ namespace Infrastructure.Data
        public StoreContext(DbContextOptions options) : base(options){} 
 
        public DbSet<Product> Products { get; set; }     
+
+       public DbSet<ProductBrand> ProductBrands { get; set; } 
+
+       public DbSet<ProductType> ProductTypes { get; set; }    
         
     }
 }
