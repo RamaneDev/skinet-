ce6e71c5daf5033e0e7c16618fc89e49994b54aa
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-03-24 10:40:15 +0100

    Add Cors for https://localhost:4200

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,13 @@ namespace API
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
