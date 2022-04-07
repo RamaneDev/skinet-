a3ece5513001d0214618c9c1388f1e221a9f07b3
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-04-07 21:26:10 +0200

    add routing -> pages

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -7,7 +8,9 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [NavBarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+   
   ],
   exports:[NavBarComponent]
 })
