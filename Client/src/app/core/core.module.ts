93fecd39f5a1ba91d5a9fea41eff46de3e706a4d
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-04-13 12:22:56 +0200

    import BreadcrumbModule & setting up breadcrumb title for each route

--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -1,3 +1,4 @@
+import { BreadcrumbModule } from 'xng-breadcrumb';
 import { ToastrModule } from 'ngx-toastr';
 import { RouterModule } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -6,20 +7,22 @@ import { CommonModule } from '@angular/common';
 import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
+  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
     RouterModule,
+    BreadcrumbModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       preventDuplicates: true
     })
    
   ],
-  exports:[NavBarComponent]
+  exports:[NavBarComponent, SectionHeaderComponent]
 })
 export class CoreModule { }
