5278bf9a1578d0b55a40399c23736651c72bcf51
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-04-26 20:44:53 +0200

    set lazy loading for checkout m

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
   {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule), data: {breadcrumb: 'Shop'}},
   {path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule), data: {breadcrumb: 'Basket'}},
+  {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule), data: {breadcrumb: 'checkout'}},
   {path: '**', redirectTo:'not-found', pathMatch:'full'}
 ];
 
