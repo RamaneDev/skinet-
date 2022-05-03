87abdabd2088a1cbf01e6cae5c91d1f01b705c0b
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-05-03 15:16:58 +0200

    Implementing Authentification

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,9 +1,11 @@
+using System.Text;
 using Core.Entities.Identity;
 using Infrastructure.Identity;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Extensions
 {
@@ -17,7 +19,17 @@ namespace API.Extensions
             builder.AddEntityFrameworkStores<StoreIdentityDbContext>();
             builder.AddSignInManager<SignInManager<StoreUser>>();
 
-            services.AddAuthentication();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(option => {
+                  option.TokenValidationParameters = new TokenValidationParameters
+                  {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Token:Key"])),
+                    ValidIssuer = config["Token:Issuer"],
+                    ValidateIssuer = true,
+                    ValidateAudience = false
+                  };
+              });
 
             return services;
         }
--- API/Startup.cs
+++ API/Startup.cs
@@ -73,7 +73,9 @@ namespace API
             //app.UseMyMiddleware2();                  we can specify filters class in attribute for actions
             //                                         or in attribute of the controller to apply to all actions
             //                                         in this controller
-            // exp of calling : Midl1 -> filter 1 -> filter 2 -> MVC API -> filter 2 -> filter 1 -> Midl1            
+            // exp of calling : Midl1 -> filter 1 -> filter 2 -> MVC API -> filter 2 -> filter 1 -> Midl1
+
+            app.UseAuthentication();            
 
             app.UseAuthorization();
 
