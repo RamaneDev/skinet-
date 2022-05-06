c122ef2b368f531985466545fd4b85e8d6fa97cc
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-05-07 01:12:15 +0200

    Adding Account feature(module,service, module-routing, login c & register c

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { TestErrorComponent } from './core/test-error/test-error.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { skip } from 'rxjs';
 
 const routes: Routes = [
   {path: '', component:HomeComponent, data: {breadcrumb: 'Home'}},
@@ -13,6 +14,7 @@ const routes: Routes = [
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule), data: {breadcrumb: 'Shop'}},
   {path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule), data: {breadcrumb: 'Basket'}},
   {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule), data: {breadcrumb: 'checkout'}},
+  {path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule), data: {breadcrumb: {skip: true}}},
   {path: '**', redirectTo:'not-found', pathMatch:'full'}
 ];
 
