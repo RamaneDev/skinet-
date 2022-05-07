eb246f954ba21da76a730cf752429a6d2a07f2bf
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-05-08 01:53:41 +0200

    updating nav-bar after login

--- Client/src/app/core/nav-bar/nav-bar.component.ts
+++ Client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AccountService } from 'src/app/account/account.service';
 import { BasketService } from 'src/app/basket/basket.service';
 import { IBasket } from 'src/app/shared/models/Basket';
+import { IUser } from 'src/app/shared/models/User';
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,11 +13,13 @@ import { IBasket } from 'src/app/shared/models/Basket';
 export class NavBarComponent implements OnInit {
  
   basket$: Observable<IBasket>;
+  currentUser$: Observable<IUser>;
 
-  constructor(private basketservice: BasketService) { }
+  constructor(private basketservice: BasketService, private accountService: AccountService ) { }
 
   ngOnInit(): void {
     this.basket$ = this.basketservice.basket$;
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
 }
