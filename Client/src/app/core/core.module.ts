55d0a40dfc00bf6e4b3656ac37ae0332297ccdce
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-05-08 14:52:56 +0200

    using BsdropdownModule in Core modue from SharedModule (to provide logout, view basket, view orders functionalities)

--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -8,6 +8,7 @@ import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { SectionHeaderComponent } from './section-header/section-header.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -20,6 +21,7 @@ import { SectionHeaderComponent } from './section-header/section-header.componen
   imports: [
     CommonModule,
     RouterModule,
+    SharedModule,
     BreadcrumbModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
