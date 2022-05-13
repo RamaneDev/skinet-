51df5a85513f204676bebca58c8dfb3d51246581
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-05-13 23:43:34 +0200

    Adding AuthGuard to protect checkout ihm  from unlogged users

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -4,17 +4,27 @@ import { TestErrorComponent } from './core/test-error/test-error.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { skip } from 'rxjs';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component:HomeComponent, data: {breadcrumb: 'Home'}},
+  
   {path: 'test-error', component:TestErrorComponent, data: {breadcrumb: 'Test Errors'}},
+  
   {path: 'not-found', component:NotFoundComponent, data: {breadcrumb: 'Not Found'}},
+  
   {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
+  
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule), data: {breadcrumb: 'Shop'}},
+  
   {path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule), data: {breadcrumb: 'Basket'}},
-  {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule), data: {breadcrumb: 'checkout'}},
+  
+  {path: 'checkout', canActivate:[AuthGuard], 
+                     loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule), 
+                     data: {breadcrumb: 'checkout'}},
+  
   {path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule), data: {breadcrumb: {skip: true}}},
+  
   {path: '**', redirectTo:'not-found', pathMatch:'full'}
 ];
 
