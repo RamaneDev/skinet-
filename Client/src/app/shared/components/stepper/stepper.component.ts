78bde2e4d84250ab6001be93cadd22941156b2da
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-05-31 12:38:49 +0200

    Creating a component for each step and using them

--- Client/src/app/shared/components/stepper/stepper.component.ts
+++ Client/src/app/shared/components/stepper/stepper.component.ts
@@ -17,8 +17,7 @@ export class StepperComponent extends CdkStepper implements OnInit {
   }
 
   onClick(index: number) {
-    this.selectedIndex = index;
-    console.log(this.selectedIndex);
+    this.selectedIndex = index;    
   }
 
 }
