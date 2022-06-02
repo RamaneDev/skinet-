ba0e4554f6a66584ae80f34c63413dc56a8e13b2
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-06-02 13:25:13 +0200

    Adding Success Markup page & redirect user to Success page after ordering

--- Client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ Client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { NavigationExtras, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { BasketService } from 'src/app/basket/basket.service';
 import { IBasket } from 'src/app/shared/models/Basket';
@@ -16,7 +17,8 @@ export class CheckoutPaymentComponent implements OnInit {
   @Input() checkoutForm: FormGroup;
   constructor(private basketService: BasketService, 
               private checkoutService: CheckoutService, 
-              private toastr: ToastrService) { }
+              private toastr: ToastrService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -27,7 +29,10 @@ export class CheckoutPaymentComponent implements OnInit {
     this.checkoutService.createOrder(orderTocreate).subscribe({
       next: (order: IOrder) => { this.toastr.success('Order created successfully');
                                  this.basketService.deleteLocalBasket(basket.id);
-                                 console.log(order); },
+                                 console.log(order); 
+                                 const navigationExtras: NavigationExtras = {state: order};
+                                 this.router.navigate(['checkout/success'], navigationExtras);
+                                },
       error: error => {console.log(error.message);
                        this.toastr.error(error.message)}
          });
--- Client/src/app/checkout/checkout-success/checkout-success.component.ts
+++ Client/src/app/checkout/checkout-success/checkout-success.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IOrder } from 'src/app/shared/models/Order';
 
 @Component({
   selector: 'app-checkout-success',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutSuccessComponent implements OnInit {
 
-  constructor() { }
+  order: IOrder;
+  
+  constructor(private router: Router) { 
+    const navigation = this.router.getCurrentNavigation();
+    const state = navigation && navigation.extras && navigation.extras.state;
+
+    if(state) {
+      this.order = state as IOrder;
+    }
+  }
 
   ngOnInit(): void {
+   
   }
 
 }
