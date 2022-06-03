67a4abba6af0d3f9dd974a6d4414532d8e3dfec4
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-06-03 05:56:33 +0200

    Getting & display the list of orders

--- Client/src/app/orders/orders.component.ts
+++ Client/src/app/orders/orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IOrder } from '../shared/models/Order';
+import { OrdersService } from './orders.service';
 
 @Component({
   selector: 'app-orders',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orders.component.scss']
 })
 export class OrdersComponent implements OnInit {
-
-  constructor() { }
+  
+  orders: IOrder[];
+  
+  constructor(private orderService: OrdersService) { }
 
   ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders() {
+    this.orderService.getOrders().subscribe({
+      next: (orders: IOrder[]) => this.orders = orders,
+      error: (err) => console.log(err)
+    });
   }
 
 }
--- Client/src/app/orders/orders.service.ts
+++ Client/src/app/orders/orders.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService {
+  private baseUrl = environment.apiUrl;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getOrders() {
+    return this.http.get(this.baseUrl + 'orders');
+  }
 }
