2bbd72b3965314679249ecbc949f5779593b3969
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-06-06 01:05:19 +0200

    Adding stripe elements and handling errors from stripe

--- Client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ Client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { NavigationExtras, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
@@ -7,21 +7,60 @@ import { IBasket } from 'src/app/shared/models/Basket';
 import { IOrder, IOrderToCreate } from 'src/app/shared/models/Order';
 import { CheckoutService } from '../checkout.service';
 
+declare var Stripe;
+
 @Component({
   selector: 'app-checkout-payment',
   templateUrl: './checkout-payment.component.html',
   styleUrls: ['./checkout-payment.component.scss']
 })
-export class CheckoutPaymentComponent implements OnInit {
+export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
 
   @Input() checkoutForm: FormGroup;
+  @ViewChild('cardNumber', {static: true}) cardNumberElement: ElementRef;
+  @ViewChild('cardExpiry', {static: true}) cardExpiryElement: ElementRef;
+  @ViewChild('cardCvc', {static: true})       cardCvcElement: ElementRef;
+
+  stripe:any;
+  cardNumer:any;
+  cardExpiry:any;
+  cardCvc:any;
+  cardErrors: any;
+  cardHandler = this.onChange.bind(this);
+  
+  
   constructor(private basketService: BasketService, 
               private checkoutService: CheckoutService, 
               private toastr: ToastrService,
-              private router: Router) { }
+              private router: Router) { } 
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    this.cardNumer.destroy();
+    this.cardExpiry.destroy();
+    this.cardCvc.destroy();
   }
+
+  onChange({error}) {
+    this.cardErrors = (error)?error.message: null;
+  }
+  
+  ngAfterViewInit(): void {
+    this.stripe = Stripe('pk_test_51L7GLBJzN45eOcHhSpoMyVsOj3paUhVZv0YaiYCLQvnqL5lzgAjfX6DSPgFfWsLjUWRJF8799oBJHlNhtC2mfpAY00KiKetYNk');
+    const elements = this.stripe.elements();
+
+    this.cardNumer = elements.create('cardNumber');
+    this.cardNumer.mount(this.cardNumberElement.nativeElement);
+    this.cardNumer.addEventListener('change', this.cardHandler);
+
+    this.cardExpiry = elements.create('cardExpiry');
+    this.cardExpiry.mount(this.cardExpiryElement.nativeElement);
+    this.cardExpiry.addEventListener('change', this.cardHandler);
+
+    this.cardCvc = elements.create('cardCvc');
+    this.cardCvc.mount(this.cardCvcElement.nativeElement);
+    this.cardCvc.addEventListener('change', this.cardHandler);
+  }
+
 
   submitOrder() {
     const basket = this.basketService.getCurrentBasketValue();
